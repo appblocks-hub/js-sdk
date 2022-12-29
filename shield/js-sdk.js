3e9b894fe3fe2d92fab501fac978a080103c03c5
Jishnu <jishnu.nb@neoito.com>
2022-12-29 12:50:53 +0530

    feature: added verifyLoginWithoutRedirect

--- shield/js-sdk.js
+++ shield/js-sdk.js
@@ -1,236 +1,246 @@
-import qs from 'query-string'
+import qs from "query-string";
 
-const base = window.location.origin
-let clientId = null
+const base = window.location.origin;
+let clientId = null;
 const authorizationEndpoint =
   process && process.env && process.env.SHIELD_AUTH_URL
     ? process.env.SHIELD_AUTH_URL
-    : 'https://shield.appblocks.com/'
+    : "https://shield.appblocks.com/";
 
 const getCodeInUrl = function () {
-  const parsedQuery = qs.parseUrl(window.location.href)
-  const code = parsedQuery.query.code
-  return code
-}
+  const parsedQuery = qs.parseUrl(window.location.href);
+  const code = parsedQuery.query.code;
+  return code;
+};
 
 class TokenStore {
   constructor() {
     if (!getCodeInUrl()) {
-      this.initRefreshCycle()
+      this.initRefreshCycle();
     }
   }
   get clientId() {
-    return this._client_id
+    return this._client_id;
   }
   set clientId(id) {
     if (id) {
-      this._client_id = id
+      this._client_id = id;
     }
   }
 
-  t
-  rt
-  te
-  _client_id = null
-  sendRefreshBefore = 10000
-  timeoutHandle
+  t;
+  rt;
+  te;
+  _client_id = null;
+  sendRefreshBefore = 10000;
+  timeoutHandle;
   setToken(token) {
-    this.t = token
-    localStorage.setItem('_ab_t', token)
+    this.t = token;
+    localStorage.setItem("_ab_t", token);
   }
   initRefreshCycle() {
-    clearTimeout(this.timeoutHandle)
-    let expiresIn = this.getExpiry()
-    console.log('expires in = ', expiresIn)
-    if (!expiresIn) return false
-    expiresIn *= 1000
+    clearTimeout(this.timeoutHandle);
+    let expiresIn = this.getExpiry();
+    console.log("expires in = ", expiresIn);
+    if (!expiresIn) return false;
+    expiresIn *= 1000;
 
-    let timer = expiresIn - new Date().getTime()
+    let timer = expiresIn - new Date().getTime();
     if (!timer || timer < this.sendRefreshBefore || isNaN(timer)) {
-      if (!timer) console.log('!timer')
+      if (!timer) console.log("!timer");
       if (timer < this.sendRefreshBefore)
-        console.log('less than', this.sendRefreshBefore)
-      if (isNaN(timer)) console.log('isNan')
+        console.log("less than", this.sendRefreshBefore);
+      if (isNaN(timer)) console.log("isNan");
       console.log(
-        'invalid expiry time ',
+        "invalid expiry time ",
         new Date().getTime(),
         expiresIn,
         timer
-      )
-      return null
+      );
+      return null;
     }
-    timer = parseInt(timer) - this.sendRefreshBefore
-    console.log('valid expiry time ', new Date().getTime(), expiresIn, timer)
+    timer = parseInt(timer) - this.sendRefreshBefore;
+    console.log("valid expiry time ", new Date().getTime(), expiresIn, timer);
     this.timeoutHandle = setTimeout(() => {
-      refreshAccessToken()
-    }, timer)
+      refreshAccessToken();
+    }, timer);
   }
   setExpiry(timestamp) {
-    this.te = timestamp
-    localStorage.setItem('_ab_t_e', timestamp)
+    this.te = timestamp;
+    localStorage.setItem("_ab_t_e", timestamp);
   }
   getExpiry() {
-    return this.te || localStorage.getItem('_ab_t_e')
+    return this.te || localStorage.getItem("_ab_t_e");
   }
   removeToken(token) {
-    this.t = token
-    localStorage.removeItem('_ab_t')
+    this.t = token;
+    localStorage.removeItem("_ab_t");
   }
   setRefreshToken(token) {
-    this.rt = token
-    localStorage.setItem('_ab_rt', token)
+    this.rt = token;
+    localStorage.setItem("_ab_rt", token);
   }
   removeRefreshToken(token) {
-    this.rt = token
-    localStorage.removeItem('_ab_rt')
+    this.rt = token;
+    localStorage.removeItem("_ab_rt");
   }
   getToken() {
-    return this.t || localStorage.getItem('_ab_t')
+    return this.t || localStorage.getItem("_ab_t");
   }
   getRefreshToken() {
-    return this.rt || localStorage.getItem('_ab_rt')
+    return this.rt || localStorage.getItem("_ab_rt");
   }
   clearTokens() {
-    this.removeRefreshToken()
-    this.removeToken()
+    this.removeRefreshToken();
+    this.removeToken();
   }
 }
 
-const tokenStore = new TokenStore()
+const tokenStore = new TokenStore();
 
 const refreshAccessToken = async () => {
-  console.log('calling refresh access token')
-  const server = `${authorizationEndpoint}refresh-token`
+  console.log("calling refresh access token");
+  const server = `${authorizationEndpoint}refresh-token`;
   try {
     const res = await fetch(server, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${tokenStore.getToken()} ${tokenStore.getRefreshToken()}`,
       },
-    })
-    const data = await res.json()
+    });
+    const data = await res.json();
     if (data && data.data.AccessToken) {
-      console.log('data is ', data.data)
-      tokenStore.setToken(data.data.AccessToken)
-      tokenStore.setExpiry(data.data.AtExpires)
-      tokenStore.setRefreshToken(data.data.RefreshToken)
-      tokenStore.initRefreshCycle()
+      console.log("data is ", data.data);
+      tokenStore.setToken(data.data.AccessToken);
+      tokenStore.setExpiry(data.data.AtExpires);
+      tokenStore.setRefreshToken(data.data.RefreshToken);
+      tokenStore.initRefreshCycle();
     } else if (data.status === 401) {
-      console.log('expired token')
-      tokenStore.clearTokens()
-      await verifyLogin()
+      console.log("expired token");
+      tokenStore.clearTokens();
+      await verifyLogin();
       // await logout()
       // verifyLogin();
     }
   } catch (error) {
-    console.log('error in refreshing = ', error)
+    console.log("error in refreshing = ", error);
     // await logout()
     // verifyLogin();
   }
-}
+};
 
 export const logout = async () => {
-  await shieldLogout()
-  tokenStore.removeRefreshToken()
-  tokenStore.removeToken()
+  await shieldLogout();
+  tokenStore.removeRefreshToken();
+  tokenStore.removeToken();
 
-  await verifyLogin()
-}
-export const verifyLogin = async (mode = 'login') => {
-  let token = tokenStore.getToken()
-  if (!token) {
-    const authorizationUrl = getAuthUrl(mode)
-    window.location = authorizationUrl
-  } else {
-    const isValid = await validateAccessToken()
-    if (!isValid) {
-      const authorizationUrl = getAuthUrl(mode)
-      window.location = authorizationUrl
-    }
-    return isValid
+  await verifyLogin();
+};
+export const verifyLogin = async (mode = "login") => {
+  const isValidToken = await initValidation(mode);
+  if (!isValidToken) {
+    tokenStore.clearTokens();
+    const authorizationUrl = getAuthUrl(mode);
+    window.location = authorizationUrl;
   }
-}
+  return isValidToken;
+};
+export const verifyLoginWithoutRedirect = async () => {
+  const isVaidToken = await initValidation(mode);
+  if (!isVaidToken) {
+    tokenStore.clearTokens();
+  }
+  return isVaidToken;
+};
+const initValidation = async () => {
+  let token = tokenStore.getToken();
+  let isValid = token;
+  if (token) {
+    isValid = await validateAccessToken();
+  }
+  return !!isValid;
+};
 const validateAccessToken = async () => {
-  const server = `${authorizationEndpoint}verify-appblocks-acess-token`
+  const server = `${authorizationEndpoint}verify-appblocks-acess-token`;
   try {
     const res = await fetch(server, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${tokenStore.getToken()}`,
-        'Client-Id': tokenStore.clientId,
+        "Client-Id": tokenStore.clientId,
       },
-    })
-    const data = await res.json() // access token set to appblocks io cookie
+    });
+    const data = await res.json(); // access token set to appblocks io cookie
 
-    return data.data && data.data === 'valid'
+    return data.data && data.data === "valid";
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 const shieldLogout = async () => {
-  const server = `${authorizationEndpoint}logout`
+  const server = `${authorizationEndpoint}logout`;
   try {
     const res = await fetch(server, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${tokenStore.getToken()}`,
       },
-    })
-    const data = await res.json() // access token set to appblocks io cookie
+    });
+    const data = await res.json(); // access token set to appblocks io cookie
 
-    return data
+    return data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 const getAuthUrl = (mode) => {
   const oAuthQueryParams = {
-    response_type: 'code',
-    scope: 'user private_repo',
+    response_type: "code",
+    scope: "user private_repo",
     redirect_uri: base,
     client_id: tokenStore.clientId,
-    state: 'state123',
-  }
+    state: "state123",
+  };
 
-  const query = qs.stringify(oAuthQueryParams)
+  const query = qs.stringify(oAuthQueryParams);
 
-  const authorizationUrl = `${authorizationEndpoint}${mode}?${query}`
-  return authorizationUrl
-}
+  const authorizationUrl = `${authorizationEndpoint}${mode}?${query}`;
+  return authorizationUrl;
+};
 
 export const init = async function (id) {
-  tokenStore.clientId = id
-  const code = getCodeInUrl()
+  tokenStore.clientId = id;
+  const code = getCodeInUrl();
   // var cookie;
   if (code) {
-    const tokenData = await sendCodeToServer(code)
+    const tokenData = await sendCodeToServer(code);
     if (tokenData.success && tokenData.data) {
-      tokenStore.setToken(tokenData.data.ab_at)
-      tokenStore.setExpiry(tokenData.data.expires_in)
-      tokenStore.setRefreshToken(tokenData.data.ab_rt)
-      tokenStore.initRefreshCycle()
+      tokenStore.setToken(tokenData.data.ab_at);
+      tokenStore.setExpiry(tokenData.data.expires_in);
+      tokenStore.setRefreshToken(tokenData.data.ab_rt);
+      tokenStore.initRefreshCycle();
     }
   }
-}
+};
 
 async function sendCodeToServer(code) {
-  const server = `${authorizationEndpoint}auth/get-token?grant_type=authorization_code&code=${code}&redirect_uri=${base}`
+  const server = `${authorizationEndpoint}auth/get-token?grant_type=authorization_code&code=${code}&redirect_uri=${base}`;
   try {
     const res = await fetch(server, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    })
-    const data = await res.json() // access token set to appblocks io cookie
-    if (location.href.includes('?')) {
-      history.pushState({}, null, location.href.split('?')[0])
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json(); // access token set to appblocks io cookie
+    if (location.href.includes("?")) {
+      history.pushState({}, null, location.href.split("?")[0]);
     }
-    console.log('🚀  file: index.js  line 50  sendCodeToServer  data', data)
-    return data
+    console.log("🚀  file: index.js  line 50  sendCodeToServer  data", data);
+    return data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -241,5 +251,5 @@ export const shield = {
   getAuthUrl,
   logout,
   validateAccessToken,
-  verifyLogin,
-}
+  verifyLoginWithoutRedirect,
+};
